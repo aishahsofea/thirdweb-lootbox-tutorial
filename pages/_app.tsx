275b4f4bfe20825310d0af179a5fe8a308405247
aishahsofea <aishah.s.hanifa@gmail.com>
2022-05-07 18:26:01 +0800

    login with ethereum

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,28 @@
+import { ThirdwebProvider } from "@3rdweb/react";
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import Layout from "../components/layout";
 import "../styles/globals.css";
 
-import type { AppProps } from "next/app";
-import Layout from "../components/layout";
-import Head from "next/head";
-
 export default function MyApp({ Component, pageProps }: AppProps) {
+  const supportedChainIds = [80001];
+  const connectors = {
+    injected: {},
+  };
+
   return (
     <>
       <Head>
         <title>{pageProps.title}</title>
       </Head>
-      <Layout title={pageProps.title}>
-        <Component {...pageProps} />
-      </Layout>
+      <ThirdwebProvider
+        connectors={connectors}
+        supportedChainIds={supportedChainIds}
+      >
+        <Layout title={pageProps.title}>
+          <Component {...pageProps} />
+        </Layout>
+      </ThirdwebProvider>
     </>
   );
 }
